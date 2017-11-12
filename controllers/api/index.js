643e80a2701b9331124602f0cbe76a450853e2b6
Konstantin Gusev <guvkon@gmail.com>
2017-11-13 04:19:51 +0700

    Implement game with simple action of adding players

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,18 +1,29 @@
 "use strict";
+import {addPlayer} from "../../lib/actions";
 
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 const tokenVerification = require('../../middlewares/tokenverification');
-
-// const userRegexp = /<@[0-9A-Z]+(\|.+)?>/i;
 
 router.use('/api', tokenVerification);
 
 router.post('/api', function(req, res) {
     console.log(req.body);
+
+    const player = {
+        id: req.body.user_id,
+        name: req.body.user_name
+    };
+    global.game.dispatch(addPlayer(player));
+
+    const state = JSON.stringify(global.game.getState());
+    fs.writeFileSync('../../data/state.json', state, {charset: 'utf8'});
+
     const response = {
         response_type: "in_channel",
-        text: "Testing"
+        text: "Added new player to the game: @" + player.name,
+        link_names: true
     };
     res.json(response);
 });
--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 "use strict";
 
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+import { createStore } from 'redux';
+import gameApp from './lib/reducers';
 
-router.use(require('./controllers'));
+const state = fs.readFileSync('./data/state.json', {charset: 'utf8'});
+global.game = createStore(gameApp, JSON.parse(state));
+global.game.subscribe(
+    () => console.log(global.game.getState())
+);
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.use('/slack/game', require('./controllers'));
+app.get('/slack/test', (req, res) => {
+    res.send("Test");
+});
+
+app.listen(process.env.SERVER_PORT, function () {
+    console.log('Listening on ', this.address());
+});
+
 
 module.exports = router;
